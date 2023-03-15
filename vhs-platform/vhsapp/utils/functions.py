cac5197bf4c81cc27a1a0f23caf8ceaa041981da
Ségolène Albouy <segolene.albouy@gmail.com>
2023-03-15 15:06:41 +0100

    [IIIF] add script of image extraction

--- vhs-platform/vhsapp/utils/functions.py
+++ vhs-platform/vhsapp/utils/functions.py
@@ -2,6 +2,7 @@ import csv
 import io
 import json
 import os
+from pathlib import Path
 from uuid import uuid4
 
 import PyPDF2
@@ -245,3 +246,38 @@ def console(msg="🚨🚨🚨"):
 
     logger = logging.getLogger("django")
     logger.info(msg)
+
+
+def coerce_to_path_and_check_exist(path):
+    path = Path(path)
+    if not path.exists():
+        raise FileNotFoundError("{} does not exist".format(path.absolute()))
+    return path
+
+
+def coerce_to_path_and_create_dir(path):
+    path = Path(path)
+    path.mkdir(parents=True, exist_ok=True)
+    return path
+
+
+def get_files_from_dir(dir_path, valid_extensions=None, recursive=False, sort=False):
+    path = coerce_to_path_and_check_exist(dir_path)
+    if recursive:
+        files = [f.absolute() for f in path.glob("**/*") if f.is_file()]
+    else:
+        files = [f.absolute() for f in path.glob("*") if f.is_file()]
+
+    if valid_extensions is not None:
+        valid_extensions = (
+            [valid_extensions]
+            if isinstance(valid_extensions, str)
+            else valid_extensions
+        )
+        valid_extensions = [
+            ".{}".format(ext) if not ext.startswith(".") else ext
+            for ext in valid_extensions
+        ]
+        files = list(filter(lambda f: f.suffix in valid_extensions, files))
+
+    return sorted(files) if sort else files
--- vhs-platform/vhsapp/utils/iiif.py
+++ vhs-platform/vhsapp/utils/iiif.py
@@ -10,13 +10,16 @@ from PIL import Image
 from pikepdf import Pdf
 from requests.exceptions import SSLError
 from tripoli import IIIFValidator
+import argparse
+from pathlib import Path
+import shutil
 from urllib.parse import urlparse
 
 from django.core.exceptions import ValidationError
 from django.utils.safestring import mark_safe
 
 from vhsapp.utils.constants import APP_NAME, MANIFEST_AUTO, MANIFEST_V2
-from vhsapp.utils.functions import log, get_json
+from vhsapp.utils.functions import log, get_json, coerce_to_path_and_create_dir, console
 from vhsapp.utils.paths import MEDIA_PATH, IMG_PATH
 from vhsapp.utils.functions import get_icon, anno_btn
 from vhsapp.models.constants import VOL_ABBR, MS_ABBR, VOL, MS
@@ -289,3 +292,125 @@ def annotate_canvas(id, version, work, work_abbr, canvas, anno, num_anno):
         "motivation": ["oa:commenting", "oa:tagging"],
         "@context": "http://iiif.io/api/presentation/2/context.json",
     }
+
+
+class IIIFDownloader:
+    """Download all image resources from a list of manifest urls."""
+
+    def __init__(self, manifest_urls, output_dir, width=None, height=None):
+        self.manifest_urls = manifest_urls
+        self.output_dir = coerce_to_path_and_create_dir(output_dir)
+        self.size = self.get_formatted_size(width, height)
+
+    @staticmethod
+    def get_formatted_size(width, height):
+        if width is None:
+            size = "full" if height is None else ",{}".format(height)
+        else:
+            size = (
+                "{},".format(width) if height is None else "{},{}".format(width, height)
+            )
+        return size
+
+    def run(self):
+        for url in self.manifest_urls:
+            manifest = self.get_json(url)
+            if manifest is not None:
+                manifest_id = Path(urlparse(manifest["@id"]).path).parent.name
+                console("Processing {}...".format(manifest_id))
+                output_path = coerce_to_path_and_create_dir(
+                    self.output_dir / manifest_id
+                )
+                resources = self.get_resources(manifest)
+
+                for resource_url in resources:
+                    resource_url = "/".join(
+                        resource_url.split("/")[:-3]
+                        + [self.size]
+                        + resource_url.split("/")[-2:]
+                    )
+                    with requests.get(resource_url, stream=True) as response:
+                        response.raw.decode_content = True
+                        resrc_path = Path(urlparse(resource_url).path)
+                        name = "{}{}".format(resrc_path.parts[-5], resrc_path.suffix)
+                        output_file = output_path / name
+                        console(
+                            "Saving {}...".format(
+                                output_file.relative_to(self.output_dir)
+                            )
+                        )
+                        with open(output_file, mode="wb") as f:
+                            shutil.copyfileobj(response.raw, f)
+
+    @staticmethod
+    def get_json(url):
+        try:
+            response = requests.get(url)
+            if response.ok:
+                return response.json()
+            else:
+                response.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            log(e)
+            return None
+
+    @staticmethod
+    def get_resources(manifest):
+        try:
+            canvases = manifest["sequences"][0]["canvases"]
+            images_list = [canvas["images"] for canvas in canvases]
+            return [
+                image["resource"]["@id"] for images in images_list for image in images
+            ]
+        except KeyError as e:
+            log(e)
+            return []
+
+    @staticmethod
+    def get_resources_with_targets(manifest):
+        try:
+            canvases = manifest["sequences"][0]["canvases"]
+            images_list = [canvas["images"] for canvas in canvases]
+            return [
+                (image["resource"]["@id"], image["on"])
+                for images in images_list
+                for image in images
+            ]
+        except KeyError as e:
+            log(e)
+            return []
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Download all image resources from a list of manifest urls"
+    )
+    parser.add_argument(
+        "-f",
+        "--file",
+        nargs="?",
+        type=str,
+        required=True,
+        help="File containing manifest urls",
+    )
+    parser.add_argument(
+        "-o",
+        "--output_dir",
+        nargs="?",
+        type=str,
+        default="output",
+        help="Output directory name",
+    )
+    parser.add_argument("--width", type=int, default=None, help="Image width")
+    parser.add_argument("--height", type=int, default=None, help="Image height")
+    args = parser.parse_args()
+
+    with open(args.file, mode="r") as f:
+        manifest_urls = f.read().splitlines()
+    manifest_urls = list(filter(None, manifest_urls))
+
+    output_dir = args.output_dir if args.output_dir is not None else "output"
+    downloader = IIIFDownloader(
+        manifest_urls, output_dir=output_dir, width=args.width, height=args.height
+    )
+    downloader.run()
