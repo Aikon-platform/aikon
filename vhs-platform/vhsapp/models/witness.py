e5b020f490eda2aff0c9ee26c3b085bdcf97dc4b
Ségolène Albouy <segolene.albouy@gmail.com>
2023-04-03 17:57:32 +0200

    [IIIF] mutualize manifest generation for volumes and mss

--- vhs-platform/vhsapp/models/witness.py
+++ vhs-platform/vhsapp/models/witness.py
@@ -113,6 +113,30 @@ class Volume(models.Model):
         # Call the parent save method to save the model
         super().save(*args, **kwargs)
 
+    def get_metadata(self):
+        metadata = {
+            "Author": self.printed.author.name if self.printed.author else "No author",
+            "Number or identifier of self": self.number_identifier,
+            "Place": self.place,
+            "Date": self.date,
+            "Publishers/booksellers": self.publishers_booksellers,
+            "Description of work": self.printed.description,
+        }
+        if descriptive_elements := self.printed.descriptive_elements:
+            metadata["Descriptive elements of the content"] = descriptive_elements
+        if illustrators := self.printed.illustrators:
+            metadata["Illustrator(s)"] = illustrators
+        if engravers := self.printed.engravers:
+            metadata["Engraver(s)"] = engravers
+        if digitized_version := self.digitized_version:
+            metadata["Source of the digitized version"] = digitized_version.source
+        if comment := self.comment:
+            metadata["Comment"] = comment
+        if other_copies := self.other_copies:
+            metadata["Other copy(ies)"] = other_copies
+
+        return metadata
+
 
 class Manuscript(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
@@ -189,3 +213,28 @@ class Manuscript(models.Model):
         self.slug = slugify(self.work.title)
         # Call the parent save method to save the model
         super().save(*args, **kwargs)
+
+    def get_metadata(self):
+        metadata = {
+            "Author": self.author.name if self.author else "No author",
+            "Place of conservation": self.conservation_place,
+            "Reference number": self.reference_number,
+            "Date (century)": self.date_century,
+            "Sheet(s)": self.sheets,
+        }
+        if date_free := self.date_free:
+            metadata["Date"] = date_free
+        if origin_place := self.origin_place:
+            metadata["Place of origin"] = origin_place
+        if remarks := self.remarks:
+            metadata["Remarks"] = remarks
+        if copyists := self.copyists:
+            metadata["Copyist(s)"] = copyists
+        if miniaturists := self.miniaturists:
+            metadata["Miniaturist(s)"] = miniaturists
+        if digitized_version := self.digitized_version:
+            metadata["Source of the digitized version"] = digitized_version.source
+        if pinakes_link := self.pinakes_link:
+            metadata["External link"] = pinakes_link
+
+        return metadata
