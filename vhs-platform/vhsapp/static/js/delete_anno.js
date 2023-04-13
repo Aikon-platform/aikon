93c8aa4f089a950ae53128bf9611a2d25d0fd186
Ségolène Albouy <segolene.albouy@gmail.com>
2023-04-13 17:16:37 +0200

    [IIIF] fix and factorize annotation files

--- vhs-platform/vhsapp/static/js/delete_anno.js
+++ vhs-platform/vhsapp/static/js/delete_anno.js
@@ -1,23 +1,34 @@
+const HTTP_SAS = SAS_APP_URL.replace("https", "http");
+
+function showMessage(message) {
+    var messages = document.getElementById(idMessage);
+    messages.textContent = message;
+    messages.style.display = "block";
+}
+
+function send_deletion(idBbox){
+    const urlDelete = `${SAS_APP_URL}/annotation/destroy?uri=${HTTP_SAS}/annotation/${idBbox}`;
+    var xhr = new XMLHttpRequest();
+    xhr.open("DELETE", urlDelete, true);
+    xhr.onload = function() {
+        if (xhr.status === 204) {
+            var $td = $(`#ill_${idBbox}`).closest("td");
+            $td.fadeOut(function() {
+                $td.remove();
+            } );
+        } else {
+            idMessage = `message_${idBbox}`;
+            showMessage(`Failed to delete ${urlDelete} due to ${xhr.status}: '${xhr.statusText}'`);
+        }
+    };
+    xhr.send();
+}
+
 $(function() {
     $("[id^=bbox_]").change(function() {
-        var idBbox = $(this).attr("id").split("_").pop();
         if (this.checked) {
             if (confirm ("Êtes-vous sûr de vouloir supprimer cette image extraite ?")) {
-                urlDelete = SAS_APP_URL + "/annotation/destroy?uri=" + SAS_APP_URL.replace("https", "http") + "/annotation/" + idBbox;
-                var xhr = new XMLHttpRequest();
-                xhr.open("DELETE", urlDelete, true);
-                xhr.onload = function() {
-                    if (xhr.status === 204) {
-                        var $td = $("#ill_" + idBbox).closest("td");
-                        $td.fadeOut(function() {
-                            $td.remove();
-                        } );
-                    } else {
-                        idMessage = "message_" + idBbox;
-                        showMessage("Failed to delete " + urlDelete + " due to " + xhr.status + ": '" + xhr.statusText + "'");
-                    }
-                };
-                xhr.send();
+                send_deletion($(this).attr("id").split("_").pop())
             } else {
                 $(this).prop("checked", false);
             }
@@ -25,39 +36,18 @@ $(function() {
     } );
 
     $("#delete_illustrations").click(function() {
-        var startIndex = parseInt(document.getElementById("startIndex").value);
-        var endIndex = parseInt(document.getElementById("endIndex").value);
+        const startIndex = parseInt(document.getElementById("startIndex").value);
+        const endIndex = parseInt(document.getElementById("endIndex").value);
         if (confirm ("Êtes-vous sûr de vouloir supprimer toutes les images extraites de Vue " + startIndex + " à Vue " + endIndex + " ?")) {
             for (var i = endIndex; i >= startIndex; i--) {
                 $("[id]").filter(function() {
                     return this.id.indexOf("bbox_") !== -1;
                 }).each(function() {
-                    if (i == this.id.split("-")["2"]) {
-                        var idBbox = this.id.split("_").pop();
-                        urlDelete = SAS_APP_URL + "/annotation/destroy?uri=" + SAS_APP_URL.replace("https", "http") + "/annotation/" + idBbox;
-                        var xhr = new XMLHttpRequest();
-                        xhr.open("DELETE", urlDelete, true);
-                        xhr.onload = function() {
-                            if (xhr.status === 204) {
-                                var $td = $("#ill_" + idBbox).closest("td");
-                                $td.fadeOut(function() {
-                                    $td.remove();
-                                } );
-                            } else {
-                                idMessage = "message_" + idBbox;
-                                showMessage("Failed to delete " + urlDelete + " due to " + xhr.status + ": '" + xhr.statusText + "'");
-                            }
-                        };
-                        xhr.send();
+                    if (i === parseInt(this.id.split("-")["2"])) {
+                        send_deletion(this.id.split("_").pop())
                     }
                 } );
             }
         }
     } );
 } );
-
-function showMessage(message) {
-    var messages = document.getElementById(idMessage);
-    messages.textContent = message;
-    messages.style.display = "block";
-}
