0be7fd8aa989c60f55e4178a42ef5fc1c92784a0
Ségolène Albouy <segolene.albouy@gmail.com>
2023-04-24 11:16:28 +0200

    [SCRIPT] create scripts to import production database

--- scripts/new_db.sh
+++ scripts/new_db.sh
@@ -2,17 +2,21 @@
 
 # HOW TO USE
 # Inside the scripts/ directory, run:
-# sh new_db.sh
+# sh new_db.sh dbName
 
 # Load environment variables from .env file
 . ../vhs-platform/vhs/.env
 
-dbname=${DB_NAME}_2
+dbname=${1:-${DB_NAME}_2}
 username=${DB_USERNAME:-admin}
 
-sudo -u postgres psql -c "CREATE DATABASE $dbname;"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $dbname TO $username;"
-sudo -u postgres psql -c "\q"
+# check if the database $dbname already exists, if so, drop it
+sudo -i -u postgres psql -tc "SELECT 1 FROM pg_database WHERE datname='$dbname'" | grep -q 1 && sudo -u postgres psql -c "DROP DATABASE $dbname"
+
+sudo -i -u postgres psql -c "GRANT ALL ON SCHEMA public TO $username;"
+sudo -i -u postgres psql -c "GRANT ALL ON SCHEMA public TO public;"
+sudo -i -u postgres psql -c "CREATE DATABASE $dbname;"
+sudo -i -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $dbname TO $username;"
 
 # Set variables in .env file
 sed -i "s/DB_NAME=.*/DB_NAME=$dbname/" ../vhs-platform/vhs/.env
