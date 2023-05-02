618213c5f56a93412ac636deb874be6f61c97312
Ségolène Albouy <segolene.albouy@gmail.com>
2023-05-02 15:24:16 +0200

    [SCRIPT] update annotation script

--- scripts/annotate.sh
+++ scripts/annotate.sh
@@ -7,20 +7,26 @@
 
 # function to print colored output
 colorEcho () {
+    Color_Off="\[\033[0m\]"
+    Red="\[\033[1;91m\]"        # Red
+    Green="\[\033[1;92m\]"      # Green
+    Yellow="\[\033[1;93m\]"     # Yellow
+    Blue="\[\033[1;94m\]"       # Blue
+    Purple="\[\033[1;95m\]"     # Purple
+    Cyan="\[\033[1;96m\]"       # Cyan
     case "$1" in
-        "success") echo -e "\033[32m$2\033[0m";;
-        "error") echo -e "\033[31m$2\033[0m";;
+        "success") echo "$Green$2$Color_Off\$";;
+        "error") echo "$Red$2$Color_Off\$";;
+        "log") echo "$Yellow$2$Color_Off\$";;
+        "info") echo "$Blue$2$Color_Off\$";;
+        "warning") echo "$Purple$2$Color_Off\$";;
+        "message") echo "$Cyan$2$Color_Off\$";;
         *) echo "$2";;
     esac
 }
 
-error () {
-    colorEcho "error" "ERROR: $1"
-    exit 1
-}
-
 if test "$#" -ne 1; then
-    error "Usage: $0 <input-file-path>"
+    colorEcho "error" "Usage: $0 <input-file-path>"
 fi
 
 input_file=$1
@@ -28,21 +34,18 @@ file_name=$(basename -- "${input_file%.*}")
 
 echo "$(figlet ANNOTATOR)"
 
-# TODO EITHER change detect_vhs.py to only detect newly downloaded images OR empty input_img/ before running detect_vhs
-
 colorEcho "info" "Copying $input_file to dishas-ia ..."
-scp "$input_file" dishas-ia:yolov5/manifests/ || error "Failed to copy file to dishas-ia"
+scp "$input_file" dishas-ia:yolov5/manifests/ || colorEcho "error" "Failed to copy file to dishas-ia"
 
 colorEcho "info" "Launching detection..."
-ssh -t dishas-ia "cd yolov5 && vhs/bin/python detect_vhs.py -f manifests/$file_name.txt" || error "Failed to run detection"
+ssh -t dishas-ia "cd yolov5 && vhs/bin/python detect_vhs.py -f manifests/$file_name.txt" || colorEcho "error" "Failed to run detection"
 
 colorEcho "info" "Content of the output directory:"
 ssh eida "ls yolov5/output/"
 
-colorEcho "info" "Copying output files to eida server ..."
-scp dishas-ia:yolov5/output/* eida:vhs/vhs-platform/mediafiles/manuscripts/annotation/ || error "Failed to copy output files to eida server"
+colorEcho "info" "Copying output files to eida server..."
+scp dishas-ia:yolov5/output/* eida:vhs/vhs-platform/mediafiles/manuscripts/annotation/ || colorEcho "error" "Failed to copy output files to eida server"
+scp dishas-ia:yolov5/output/* ../mediafiles/manuscripts/annotation/ || colorEcho "error" "Failed to copy output files to local app"
 
-colorEcho "success" "All steps completed successfully."
-
-colorEcho "info" "Content of the annotation directory:"
+colorEcho "log" "Content of the annotation directory:"
 ssh eida "ls vhs/vhs-platform/mediafiles/manuscripts/annotation/"
