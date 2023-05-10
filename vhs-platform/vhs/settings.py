ac6e6487d55ac66eb30e52e9cd24279e20964724
Ségolène Albouy <segolene.albouy@gmail.com>
2023-05-10 15:05:02 +0200

    [IIIF] add celery to req + fix annotation generation

--- vhs-platform/vhs/settings.py
+++ vhs-platform/vhs/settings.py
@@ -204,3 +204,10 @@ LOGGING = {
         "verbose": {"format": "%(asctime)s - %(levelname)s - %(message)s"},
     },
 }
+
+# Celery settings
+CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
