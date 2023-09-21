6fc96f91e768c0a7b15e0a1abde41df0c6d360ca
Segolene-Albouy <segolene.albouy@gmail.com>
2023-09-21 16:14:51 +0200

    [SCRIPT] update script to create database with superuser

--- scripts/new_db.sh
+++ scripts/new_db.sh
@@ -3,12 +3,15 @@
 # HOW TO USE
 # Inside the scripts/ directory, run:
 # sh new_db.sh dbName
+# You will be asked to enter password twice
+# Restart Django to see effects
 
 # Load environment variables from .env file
 . ../app/config/.env
 
 dbname=${1:-${DB_NAME}_2}
 username=${DB_USERNAME:-admin}
+password=${DB_PASSWORD}
 
 # check if the database $dbname already exists, if so, drop it
 sudo -i -u postgres psql -tc "SELECT 1 FROM pg_database WHERE datname='$dbname'" | grep -q 1 && sudo -u postgres psql -c "DROP DATABASE $dbname"
@@ -22,8 +25,11 @@ sudo -i -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $dbname TO $userna
 sed -i "s/DB_NAME=.*/DB_NAME=$dbname/" ../app/config/.env
 
 # Empty migration directory and create new migrations
-find ../app/webapp/migrations -type f ! -name '__init__.py' -delete
+find ../app/webapp/migrations -type f ! -name '__init__.py' ! -name 'init.py' -delete
 ../venv/bin/python ../app/manage.py makemigrations
 
 # Update database schema with new migrations
 ../venv/bin/python ../app/manage.py migrate
+
+# create superuser
+../venv/bin/python ../app/manage.py createsuperuser --username=$username --email=
