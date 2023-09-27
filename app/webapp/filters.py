129aa790102fc4e39ebfd51a7472f92108ffc7c7
Fouad Aouinti <aouinti.fouad@gmail.com>
2023-09-27 09:39:14 +0200

    [FILT] add filters to exclude item

--- app/webapp/filters.py
+++ app/webapp/filters.py
@@ -36,3 +36,19 @@ def replace_nth_part_of_url(url, args):
 def img_to_iiif(img_file, args="full/full/0"):
     # args = {region}/{size}/{orientation}
     return f"{CANTALOUPE_APP_URL}/iiif/2/{img_file}/{args}/default.jpg"
+
+
+@register.filter
+def exclude_and_join(lst, item):
+    """
+    Exclude a specific item from a list and join the remaining items with underscores
+    """
+    return "_".join(str(i) for i in lst if i != item)
+
+
+@register.filter
+def exclude_item_from_list(lst, item):
+    """
+    Exclude a specific item from a list and return the remaining items
+    """
+    return [i for i in lst if i != item]
