5498ffe01cba028be5d975ed35c62dcaa415a410
Ségolène Albouy <segolene.albouy@gmail.com>
2023-12-14 11:35:04 +0100

    [FIX] remove log()

--- app/webapp/views.py
+++ app/webapp/views.py
@@ -266,7 +266,6 @@ def delete_send_anno(request, anno_ref):
 
 @csrf_exempt
 def receive_anno(request, digit_ref):
-    log(digit_ref)
     passed, digit = check_ref(digit_ref)
     if not passed:
         return JsonResponse(digit)
