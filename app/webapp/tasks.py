ee2714636d1c4436f51b6e77b11a2ce2a9b6ef86
Ségolène Albouy <segolene.albouy@gmail.com>
2023-12-15 16:27:26 +0100

    [CELERY] add test view task

--- app/webapp/tasks.py
+++ app/webapp/tasks.py
@@ -1,27 +1,28 @@
 from celery import shared_task
 
+from app.config.celery import celery_app
 from app.webapp.utils.iiif.annotation import process_anno, check_indexation_annos
 
 
-@shared_task
+@celery_app.task
 def convert_pdf_to_imgs(pdf_path, img_path):
     # TODO: pdf_to_images_conversion
     pass
 
 
-@shared_task
+@celery_app.task
 def extract_imgs_from_manifest(url, img_path, work):
     # TODO: manifest_image_extraction
     pass
 
 
-@shared_task
+@celery_app.task
 def search_similarity(exp_path, work):
     # TODO: similarity_search
     pass
 
 
-@shared_task
+@celery_app.task
 def process_anno_file(file_content, digit_id, model):
     from app.webapp.models.annotation import Digitization
 
@@ -29,9 +30,16 @@ def process_anno_file(file_content, digit_id, model):
     return process_anno(file_content, digitization, model)
 
 
-@shared_task
+@celery_app.task
 def reindex_from_file(anno_id):
     from app.webapp.models.annotation import Annotation
 
     annotation = Annotation.objects.filter(pk=anno_id).first()
     return check_indexation_annos(annotation, True)
+
+
+@celery_app.task
+def test(log_msg):
+    from app.webapp.utils.logger import log
+
+    log(log_msg)
