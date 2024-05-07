474190782c93785d8cfd67c59b1b2eae69dd621a
Fouad Aouinti <aouinti.fouad@gmail.com>
2024-05-07 20:39:22 +0200

    [MODELS] delete all corresponding witnesses when deleting a series

--- app/webapp/models/region_pair.py
+++ app/webapp/models/region_pair.py
@@ -1,6 +1,5 @@
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
-from django.contrib.auth.models import User
 from app.webapp.models.utils.functions import get_fieldname
 
 
