16ae123b2e111bb7fe8843be07effff27210fe44
Clara Grometto <Clara.Grometto@obspm.fr>
2024-07-03 10:38:26 +0200

    [UTILS] factorisation

--- app/vectorization/utils.py
+++ app/vectorization/utils.py
@@ -7,22 +7,20 @@ from app.webapp.utils.logger import log
 from app.webapp.utils.iiif.annotation import get_regions_urls
 
 
-def vectorization_request_for_one(regions):
-
+def make_vectorization_request(regions, endpoint):
     """
-    Request to the API to launch vectorization on one witness
+    Function to make a vectorization request to the specified endpoint.
     """
-
     try:
-        response = requests.post(
-            url=f"{CV_API_URL}/vectorization/start",
-            json={
-                "doc_id": regions.get_ref(),
-                "model": f"{VECTO_MODEL_EPOCH}",
-                "images": get_regions_urls(regions),
-                "callback": f"{APP_URL}/{APP_NAME}/receive-vectorization",
-            },
-        )
+        url = f"{CV_API_URL}/vectorization/{endpoint}"
+        payload = {
+            "doc_id": regions.get_ref(),
+            "model": f"{VECTO_MODEL_EPOCH}",
+            "images": get_regions_urls(regions),
+            "callback": f"{APP_URL}/{APP_NAME}/receive-vectorization",
+        }
+
+        response = requests.post(url, json=payload)
 
         print("Response:", response)
         print("Response status code:", response.status_code)
@@ -39,11 +37,8 @@ def vectorization_request_for_one(regions):
                 "error_message": f"Request failed for {regions} with status code: {response.status_code}",
                 "request_info": {
                     "method": "POST",
-                    "url": f"{CV_API_URL}/vectorization/start",
-                    "payload": {
-                        "document": regions,
-                        "callback": f"{APP_URL}/{APP_NAME}/vectorization",
-                    },
+                    "url": url,
+                    "payload": payload,
                 },
                 "response_info": {
                     "status_code": response.status_code,
@@ -56,6 +51,15 @@ def vectorization_request_for_one(regions):
     except Exception as e:
         log(f"[vectorization_request] Request failed for {regions}", e)
         return False
+
+
+def vectorization_request_for_one(regions):
+
+    """
+    Request to the API to launch vectorization on one witness
+    """
+
+    return make_vectorization_request(regions, "start")
 
 
 def vectorization_request(regions):
@@ -77,46 +81,4 @@ def delete_and_relauch_request(regions):
     Request to the API endpoint to delete imgs from the repo corresponding to doc_id + relauch the vectorization
     """
 
-    try:
-        response = requests.post(
-            url=f"{CV_API_URL}/vectorization/delete_and_relaunch",
-            json={
-                "doc_id": regions.get_ref(),
-                "model": f"{VECTO_MODEL_EPOCH}",
-                "images": get_regions_urls(regions),
-                "callback": f"{APP_URL}/{APP_NAME}/receive-vectorization",
-            },
-        )
-
-        print("Response:", response)
-        print("Response status code:", response.status_code)
-        print("Response text:", response.text)
-
-        if response.status_code == 200:
-            log(
-                f"[vectorization_request] Vectorization request sent: {response.text or ''}"
-            )
-            return True
-        else:
-            error = {
-                "source": "[vectorization_request]",
-                "error_message": f"Request failed for {regions} with status code: {response.status_code}",
-                "request_info": {
-                    "method": "POST",
-                    "url": f"{CV_API_URL}/vectorization/delete_and_relauch",
-                    "payload": {
-                        "document": regions,
-                        "callback": f"{APP_URL}/{APP_NAME}/vectorization",
-                    },
-                },
-                "response_info": {
-                    "status_code": response.status_code,
-                    "text": response.text or "",
-                },
-            }
-
-            log(error)
-            return False
-    except Exception as e:
-        log(f"[vectorization_request] Request failed for {regions}", e)
-        return False
+    return make_vectorization_request(regions, "delete_and_relaunch")
