214ba26f5bad097e5e760bfb9f33f7f828ac86fe
Segolene-Albouy <segolene.albouy@gmail.com>
2024-08-01 15:43:27 +0200

    [SETS] create proto to_json() method

--- app/webapp/models/document_set.py
+++ app/webapp/models/document_set.py
@@ -1,7 +1,14 @@
 from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
+
+from app.webapp.models.digitization import Digitization
+from app.webapp.models.series import Series
 from app.webapp.models.utils.functions import get_fieldname
+from django.urls import reverse
+
+from app.webapp.models.witness import Witness
+from app.webapp.models.work import Work
 
 
 def get_name(fieldname, plural=False):
@@ -35,3 +42,53 @@ class DocumentSet(models.Model):
 
     created_at = models.DateTimeField(blank=True, null=True, auto_now_add=True)
     updated_at = models.DateTimeField(blank=True, null=True, auto_now=True)
+
+    def get_absolute_url(self):
+        # TODO create view to edit document set without loading it
+        # return reverse("document_set", args=[self.id])
+        return ""
+
+    def get_witnesses(self):
+        if not self.wit_ids:
+            return []
+        return list(Witness.objects.filter(id__in=self.wit_ids))
+
+    def get_series(self):
+        if not self.ser_ids:
+            return []
+        return list(Series.objects.filter(id__in=self.ser_ids))
+
+    def get_digits(self):
+        if not self.digit_ids:
+            return []
+        return list(Digitization.objects.filter(id__in=self.digit_ids))
+
+    def get_works(self):
+        if not self.work_ids:
+            return []
+        return list(Work.objects.filter(id__in=self.work_ids))
+
+    def get_str_documents(self):
+        pass
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "class": self.__class__.__name__,
+            "type": get_name("Treatment"),
+            "title": self.__str__(),
+            "user_id": self.user.id,
+            "user": self.user.__str__(),
+            "url": self.get_absolute_url(),
+            "updated_at": self.updated_at.strftime("%Y-%m-%d %H:%M"),
+            "is_public": self.is_public,
+            # "metadata": {
+            #     get_name("id_nb"): self.id_nb or "-",
+            #     get_name("Work"): self.get_work_titles(),
+            #     get_name("place_name"): self.get_place_names(),
+            #     get_name("dates"): format_dates(*self.get_dates()),
+            #     get_name("page_nb"): self.get_page(),
+            #     get_name("Language"): self.get_lang_names(),
+            # },
+            # "buttons": buttons,
+        }
