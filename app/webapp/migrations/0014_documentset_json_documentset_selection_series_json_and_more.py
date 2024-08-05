22bcb97e9a6ccef7238ed342b43e409c8c35caf3
Segolene-Albouy <segolene.albouy@gmail.com>
2024-08-05 17:42:54 +0200

    [FIX] correct data migrations

--- app/webapp/migrations/0014_documentset_json_documentset_selection_series_json_and_more.py
+++ app/webapp/migrations/0014_documentset_json_documentset_selection_series_json_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("webapp", "0012_alter_documentset_title"),
+        ("webapp", "0013_alter_conservationplace_options_and_more"),
     ]
 
     operations = [
