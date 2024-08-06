ac4c3e0542924a226df438130f0f5a39b91d9c83
Segolene-Albouy <segolene.albouy@gmail.com>
2024-08-06 11:27:43 +0200

    [SIMILARITY] task for processing files

--- app/webapp/search_filters.py
+++ app/webapp/search_filters.py
@@ -2,13 +2,14 @@ from dal import autocomplete
 import django_filters
 from django.forms import DateTimeField, DateField
 from django.forms.models import ModelChoiceIteratorValue
-from django.db.models import QuerySet
+from django.db.models import QuerySet, Model
 
 from app.webapp.models.conservation_place import ConservationPlace
 from app.webapp.models.document_set import DocumentSet
 from app.webapp.models.edition import Edition, get_name as edition_name
 from app.webapp.models.language import Language
 from app.webapp.models.person import Person
+from app.webapp.models.place import Place
 from app.webapp.models.series import Series, get_name as series_name
 from app.webapp.models.treatment import Treatment, get_name as treatment_name
 from app.webapp.models.witness import Witness, get_name as witness_name
@@ -45,13 +46,11 @@ class RecordFilter(django_filters.FilterSet):
                 if isinstance(field.choices, QuerySet):
                     field_info["choices_count"] = field.choices.count()
                 else:
-                    field_info["choices"] = [
-                        {
-                            "value": serialize_choice_value(choice[0]),
-                            "label": str(choice[1]),
-                        }
-                        for choice in field.choices
-                    ]
+                    choices = []
+                    for choice in field.choices:
+                        value = serialize_choice_value(choice[0])
+                        choices.append({"value": value, "label": str(choice[1])})
+                    field_info["choices"] = choices
 
             if isinstance(field, (DateTimeField, DateField)):
                 field_info["initial"] = (
@@ -67,7 +66,6 @@ class WitnessFilter(RecordFilter):
     # TODO make autocompletion work
     edition = django_filters.ModelChoiceFilter(
         queryset=Edition.objects.all(),
-        widget=autocomplete.ModelSelect2(url="webapp:edition-autocomplete"),
     )
     contents__work = django_filters.ModelChoiceFilter(
         queryset=Work.objects.all(),
@@ -81,11 +79,10 @@ class WitnessFilter(RecordFilter):
     contents__lang = django_filters.ModelMultipleChoiceFilter(
         queryset=Language.objects.all(),
         null_value=None,
-        widget=autocomplete.ModelSelect2Multiple(url="webapp:language-autocomplete"),
     )
     contents__date_min = django_filters.RangeFilter(
         field_name="contents__date_min", label=witness_name("date_min")
-    )  # , widget=django_filters.widgets.RangeWidget(attrs={"class": "range"}))
+    )
     contents__date_max = django_filters.RangeFilter(
         field_name="contents__date_max", label=witness_name("date_max")
     )
@@ -134,11 +131,10 @@ class TreatmentFilter(RecordFilter):
 class WorkFilter(RecordFilter):
     contents__lang = django_filters.ModelChoiceFilter(
         queryset=Language.objects.all(),
-        widget=autocomplete.ModelSelect2Multiple(url="webapp:language-autocomplete"),
     )
     contents__date_min = django_filters.RangeFilter(
         field_name="contents__date_min", label=witness_name("date_min")
-    )  # , widget=django_filters.widgets.RangeWidget(attrs={"class": "range"}))
+    )
     contents__date_max = django_filters.RangeFilter(
         field_name="contents__date_max", label=witness_name("date_max")
     )
@@ -159,7 +155,12 @@ class WorkFilter(RecordFilter):
 class SeriesFilter(RecordFilter):
     edition = django_filters.ModelChoiceFilter(
         queryset=Edition.objects.all(),
-        widget=autocomplete.ModelSelect2(url="webapp:edition-autocomplete"),
+    )
+    edition__place = django_filters.ModelChoiceFilter(
+        queryset=Place.objects.all(),
+    )
+    edition__publisher = django_filters.ModelChoiceFilter(
+        queryset=Person.objects.all(),
     )
     contents__date_min = django_filters.RangeFilter(
         field_name="contents__date_min", label=witness_name("date_min")
