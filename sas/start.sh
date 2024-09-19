518eee532c85e542263fef003d5dea5eb9f97390
Segolene-Albouy <segolene.albouy@gmail.com>
2024-09-19 10:46:37 +0200

    [DOCKER] fix sas build

--- sas/start.sh
+++ sas/start.sh
@@ -1,5 +1,3 @@
 #! /bin/bash
-#SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-#cd "$SCRIPT_DIR" && mvn jetty:run
-cd /sas && mvn jetty:run
-#java -jar /sas/simple-annotation-server.war
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$SCRIPT_DIR" && mvn jetty:run
