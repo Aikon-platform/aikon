fa579c00dabedff79a90b5ad070129295cfead43
Segolene-Albouy <segolene.albouy@gmail.com>
2024-09-19 09:57:04 +0200

    [DOCKER] change sas build

--- sas/start.sh
+++ sas/start.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd "$SCRIPT_DIR" && mvn jetty:run
+#SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+#cd "$SCRIPT_DIR" && mvn jetty:run
+cd /sas && mvn jetty:run
 #java -jar /sas/simple-annotation-server.war
