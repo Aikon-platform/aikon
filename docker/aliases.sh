c9ca78349e233a6dd31e5e88357324cedd456c38
Segolene-Albouy <segolene.albouy@gmail.com>
2024-10-16 10:56:42 +0200

    [DOCKER] script to add custom aliases to server

--- docker/aliases.sh
+++ docker/aliases.sh
@@ -1,14 +1,35 @@
-"""
-git_branch() {
-    if [ -d .git ] || git rev-parse --git-dir > /dev/null 2>&1; then
-        local branch_name=$(git branch 2>/dev/null | grep '^*' | colrm 1 2)
+#!/bin/bash
 
-        if [ -n "$branch_name" ]; then
-            echo "$branch_name"
-        fi
+# Paths for aliases and functions
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+APP_ROOT="$(dirname "$SCRIPT_DIR")"
+
+add_if_not_exists() {
+    local entry="$1"
+    local file="$2"
+
+    if ! grep -qF "$entry" "$file"; then
+        echo "$entry" >> "$file"
     fi
 }
-# Command to visualize new addition to the current branch before pulling
-# Excludes the svelte folder containing minified code
-alias gdiff='git fetch && git diff $(git_branch) origin/$(git_branch) -- ":(exclude)app/webapp/static/svelte/"'
-"""
+
+BASHRC_FILE="$HOME/.bashrc"
+
+git_branch_func="git_branch() {
+    if [ -d .git ] || git rev-parse --git-dir > /dev/null 2>&1; then
+        echo $(git branch 2>/dev/null | grep '^*' | colrm 1 2)
+    fi
+}"
+
+alias_gdiff="alias gdiff='git fetch && git diff \$(git_branch) origin/\$(git_branch) -- \":(exclude)$APP_ROOT/app/webapp/static/svelte/\"'"
+alias_aikon="alias aikon='cd $APP_ROOT/docker && docker compose'"
+alias_static="alias static='aikon exec web /home/aikon/venv/bin/python /home/aikon/app/manage.py collectstatic --noinput'"
+alias_log="alias log='aikon exec web tail -n 150 -f /home/aikon/app/logs/app_log.log'"
+
+add_if_not_exists "$git_branch_func" "$BASHRC_FILE"
+add_if_not_exists "$alias_gdiff" "$BASHRC_FILE"
+add_if_not_exists "$alias_aikon" "$BASHRC_FILE"
+add_if_not_exists "$alias_static" "$BASHRC_FILE"
+add_if_not_exists "$alias_log" "$BASHRC_FILE"
+
+source "$BASHRC_FILE"
