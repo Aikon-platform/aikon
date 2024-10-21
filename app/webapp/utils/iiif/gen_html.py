b33093425e0eee011ed4e61bc2030f699509ce72
Segolene-Albouy <segolene.albouy@gmail.com>
2024-10-21 20:35:37 +0200

    [FORM] better view source btn

--- app/webapp/utils/iiif/gen_html.py
+++ app/webapp/utils/iiif/gen_html.py
@@ -21,51 +21,42 @@ def regions_btn(obj, action="view"):
     btn = f"{get_action(action, 'upper')}"
 
     if action == "view":
-        color = "#EFB80B"
         icon = get_icon("eye")
         # The link redirects to Mirador with no regions (Digitization) or automatic regions (Regions)
         link = f"{SAS_APP_URL}/indexView.html?iiif-content={obj.gen_manifest_url(version=MANIFEST_V1)}"
     elif action == "auto-view":
-        color = "#EFB80B"
         icon = get_icon("eye")
         # The link redirects to Mirador with no regions (Digitization) or automatic regions (Regions)
         link = f"{SAS_APP_URL}/indexView.html?iiif-content={obj.gen_manifest_url(version=MANIFEST_V1)}"
     elif action == "edit":
-        color = "#008CBA"
         icon = get_icon("pen-to-square")
         # The link redirects to the edit regions page (show_regions() view)
         link = f"{APP_URL}/{APP_NAME}/{obj.get_ref()}/show/"
     elif action == "final":
-        color = "#4CAF50"
         icon = get_icon("check")
         # The link redirects to Mirador with corrected regions (Regions)
         link = f"{SAS_APP_URL}/indexView.html?iiif-content={obj.gen_manifest_url(version=MANIFEST_V2)}"
     elif action == "similarity":
-        color = "#24d1b7"
         icon = get_icon("code-compare")
         link = f"{APP_URL}/{APP_NAME}/{obj.get_ref()}/show-similarity"
     elif action == "regions":
-        color = "#008CBA"
         icon = get_icon("eye")
         link = f"{APP_URL}/{APP_NAME}/{obj.get_ref()}/show-all-regions"
     elif action == "vectors":
-        color = "#EFB80B"
         icon = get_icon("arrows")
         link = f"{APP_URL}/{APP_NAME}/{obj.get_ref()}/show-vectorization"
     elif action == "vectorization":
-        color = "#EFB80B"
         icon = get_icon("pen-to-square")
         link = f"{APP_URL}/{APP_NAME}/run-vectorization/{obj.get_ref()}"
     else:
         # When the button is not supposed to redirects to anything
         link = "#"
         btn = get_action(action, "upper")
-        color = "#878787"
         icon = get_icon("eye-slash")
         disabled = "disabled"
 
     return (
-        f"<a href='{link}' class='button active is-link is-inverted is-small my-2' role='button' "
+        f"<a href='{link}' class='button active is-link is-small my-2' role='button' "
         f"aria-pressed='true' target='_blank' {disabled}>{icon} {btn}</a>"
     )
 
