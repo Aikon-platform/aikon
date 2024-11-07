29102bd564a6392239b0e94492a95ddc6baf5198
Segolene-Albouy <segolene.albouy@gmail.com>
2024-11-07 15:00:55 +0100

    [REGIONS] default model relative to app

--- app/regions/const.py
+++ app/regions/const.py
@@ -1,8 +1,8 @@
-from app.config.settings import WEBAPP_NAME
+from app.config.settings import WEBAPP_NAME, APP_NAME
 from app.webapp.utils.paths import BASE_DIR
 
 MODULE_NAME = "regions"
 MODULE_DIR = BASE_DIR / WEBAPP_NAME / MODULE_NAME
 
 # TODO retrieve api model instead
-EXTRACTOR_MODEL = "best_eida.pt"
+EXTRACTOR_MODEL = f"best_{APP_NAME.lower()}.pt"
