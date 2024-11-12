b3f7e163b628d37b9b2241ad9c708a2c24f92e75
Segolene-Albouy <segolene.albouy@gmail.com>
2024-11-12 11:31:53 +0100

    [SAS] debug manifest upload

--- sas/src/main/java/uk/org/llgc/annotation/store/servlets/ManifestUpload.java
+++ sas/src/main/java/uk/org/llgc/annotation/store/servlets/ManifestUpload.java
@@ -16,6 +16,8 @@ import java.io.FileWriter;
 import java.io.FileReader;
 import java.io.FilenameFilter;
 import java.io.BufferedReader;
+import java.net.URL;
+import java.net.HttpURLConnection;
 
 import java.net.URL;
 
@@ -37,79 +39,91 @@ import uk.org.llgc.annotation.store.AnnotationUtils;
 import uk.org.llgc.annotation.store.StoreConfig;
 
 public class ManifestUpload extends HttpServlet {
-	protected static Logger _logger = LogManager.getLogger(ManifestUpload.class.getName());
-	protected AnnotationUtils _annotationUtils = null;
-	protected StoreAdapter _store = null;
+    protected static Logger _logger = LogManager.getLogger(ManifestUpload.class.getName());
+    protected AnnotationUtils _annotationUtils = null;
+    protected StoreAdapter _store = null;
 
-	public void init(final ServletConfig pConfig) throws ServletException {
-		super.init(pConfig);
-		Encoder tEncoder = StoreConfig.getConfig().getEncoder();
-		_annotationUtils = new AnnotationUtils(new File(super.getServletContext().getRealPath("/contexts")), tEncoder);
-		_store = StoreConfig.getConfig().getStore();
-		_store.init(_annotationUtils);
-	}
+    public void init(final ServletConfig pConfig) throws ServletException {
+        super.init(pConfig);
+        Encoder tEncoder = StoreConfig.getConfig().getEncoder();
+        _annotationUtils = new AnnotationUtils(new File(super.getServletContext().getRealPath("/contexts")), tEncoder);
+        _store = StoreConfig.getConfig().getStore();
+        _store.init(_annotationUtils);
+    }
 
-	public void doPost(final HttpServletRequest pReq, final HttpServletResponse pRes) throws IOException {
-		String tID = "";
-		Map<String, Object> tManifestJson = null;
-		if (pReq.getParameter("uri") != null) {
-			tID = pReq.getParameter("uri");
-			tManifestJson = (Map<String,Object>)JsonUtils.fromInputStream(new URL(tID).openStream());
-		} else {
-			InputStream tManifestStream = pReq.getInputStream();
-            /*java.io.BufferedReader tReader = new java.io.BufferedReader(new java.io.InputStreamReader(tManifestStream));
-            String tLine = tReader.readLine();
-            while (tLine != null) {
-                System.out.println(tLine);
-                tLine = tReader.readLine();
-            }*/
+    // Add proxy configuration
+    static {
+        String httpProxy = System.getenv("HTTP_PROXY");
+        String httpsProxy = System.getenv("HTTPS_PROXY");
+        String noProxy = System.getenv("NO_PROXY");
 
-			tManifestJson = (Map<String,Object>)JsonUtils.fromInputStream(tManifestStream);
-		}
+        if (httpProxy != null && !httpProxy.isEmpty()) {
+            try {
+                URL proxyUrl = new URL(httpProxy);
+                System.setProperty("http.proxyHost", proxyUrl.getHost());
+                System.setProperty("http.proxyPort", String.valueOf(proxyUrl.getPort()));
+            } catch (Exception e) {
+                _logger.error("Failed to parse HTTP_PROXY: " + e.getMessage());
+            }
+        }
 
-        Manifest tManifest = new Manifest(tManifestJson, null);
-		String tShortId = _store.indexManifest(tManifest);
-        Map<String,Object> tJson = new HashMap<String,Object>();
-        Map<String,String> tLinks = new HashMap<String,String>();
-        tJson.put("loaded", tLinks);
-        tLinks.put("uri", tManifest.getURI());
-        tLinks.put("short_id", tManifest.getShortId());
+        if (httpsProxy != null && !httpsProxy.isEmpty()) {
+            try {
+                URL proxyUrl = new URL(httpsProxy);
+                System.setProperty("https.proxyHost", proxyUrl.getHost());
+                System.setProperty("https.proxyPort", String.valueOf(proxyUrl.getPort()));
+            } catch (Exception e) {
+                _logger.error("Failed to parse HTTPS_PROXY: " + e.getMessage());
+            }
+        }
 
-        pRes.setContentType("application/json");
-        pRes.setCharacterEncoding("UTF-8");
-        JsonUtils.write(pRes.getWriter(), tJson);
-	}
+        if (noProxy != null && !noProxy.isEmpty()) {
+            System.setProperty("http.nonProxyHosts", noProxy.replace(",", "|"));
+        }
+    }
 
-	// if asked for without path then return collection of manifests that are loaded
-	public void doGet(final HttpServletRequest pReq, final HttpServletResponse pRes) throws IOException {
-		String tRequestURI = pReq.getRequestURI();
-		String[] tSplitURI = tRequestURI.split("/");
+    public void doPost(final HttpServletRequest pReq, final HttpServletResponse pRes) throws IOException {
+        String tID = "";
+        Map<String, Object> tManifestJson = null;
+        if (pReq.getParameter("uri") != null) {
+            tID = pReq.getParameter("uri");
+            _logger.info("Fetching manifest from: " + tID);
 
-		// Return collection
-		List<Manifest> tManifests = _store.getManifests();
+            // Use HttpURLConnection with timeout settings
+            URL url = new URL(tID);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setConnectTimeout(30000); // 30 seconds
+            conn.setReadTimeout(30000);    // 30 seconds
 
-		Map<String,Object> tCollection = new HashMap<String,Object>();
+            try {
+                tManifestJson = (Map<String,Object>)JsonUtils.fromInputStream(conn.getInputStream());
+            } catch (Exception e) {
+                _logger.error("Failed to fetch manifest: " + e.getMessage(), e);
+                pRes.sendError(HttpServletResponse.SC_BAD_GATEWAY, "Failed to fetch manifest: " + e.getMessage());
+                return;
+            } finally {
+                conn.disconnect();
+            }
+        } else {
+            InputStream tManifestStream = pReq.getInputStream();
+            tManifestJson = (Map<String,Object>)JsonUtils.fromInputStream(tManifestStream);
+        }
 
-		tCollection.put("@context", "http://iiif.io/api/presentation/2/context.json");
-		tCollection.put("@id", StoreConfig.getConfig().getBaseURI(pReq) + "/annotation//collection/managed.json");
-		tCollection.put("@type", "sc:Collection");
-		tCollection.put("label","Collection of all manifests known by this annotation server");
+        try {
+            Manifest tManifest = new Manifest(tManifestJson, null);
+            String tShortId = _store.indexManifest(tManifest);
+            Map<String,Object> tJson = new HashMap<String,Object>();
+            Map<String,String> tLinks = new HashMap<String,String>();
+            tJson.put("loaded", tLinks);
+            tLinks.put("uri", tManifest.getURI());
+            tLinks.put("short_id", tManifest.getShortId());
 
-		List<Map<String,Object>> tMembers = new ArrayList<Map<String,Object>>();
-		for (Manifest tManifest : tManifests) {
-			Map<String, Object> tManifestJson = new HashMap<String,Object>();
-			tManifestJson.put("@id", tManifest.getURI());
-			tManifestJson.put("@type", "sc:Manifest");
-
-			tMembers.add(tManifestJson);
-		}
-
-		tCollection.put("members", tMembers);
-		tCollection.put("manifests", tMembers);
-
-		pRes.setStatus(HttpServletResponse.SC_CREATED);
-		pRes.setContentType("application/ld+json; charset=UTF-8");
-		pRes.setCharacterEncoding("UTF-8");
-		JsonUtils.write(pRes.getWriter(), tCollection);
-	}
+            pRes.setContentType("application/json");
+            pRes.setCharacterEncoding("UTF-8");
+            JsonUtils.write(pRes.getWriter(), tJson);
+        } catch (Exception e) {
+            _logger.error("Failed to process manifest: " + e.getMessage(), e);
+            pRes.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Failed to process manifest: " + e.getMessage());
+        }
+    }
 }
