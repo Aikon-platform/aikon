d72fbe66a463480e666a1e6332fc00581f0d61fd
paul k <paul.kervegan@hotmail.fr>
2025-02-27 15:00:21 +0100

    [SCRIPT] change message

--- front/scripts/setup_db.sh
+++ front/scripts/setup_db.sh
@@ -13,6 +13,7 @@ db_psw=$(get_env_value "POSTGRES_PASSWORD" "$APP_ENV")
 echo_title "DATABASE GENERATION"
 
 color_echo yellow "\n⚠️ The script will create a new database named $db_name and an app user named $db_user with password $db_psw (all defined in aikon_dir/front/config/.env)"
+color_echo yellow "You will be prompted several times for your postgres password. If you enter the wrong password, you need to quit start the Database initialization step again (otherwise, an install step will skip and the DB creation will fail)."
 options=("ok")
 printf "%s\n" "${options[@]}" | fzy
 bash "$SCRIPT_DIR"/new_db.sh "$db_name"
