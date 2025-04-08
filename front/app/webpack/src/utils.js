f00fb73dbc0caa66a9bf2c7eec338fb95b28c4a5
Rémy DELANAUX <delanaux@imj-prg.fr>
2025-04-08 09:18:36 +0200

    [EXPORT/IMPORT/JSON API] Basic UI changes for import/export

--- front/app/webpack/src/utils.js
+++ front/app/webpack/src/utils.js
@@ -79,7 +79,7 @@ export function parseData(elementId) {
     return JSON.parse(document.getElementById(elementId).textContent);
 }
 
-export function showMessage(msg, title = null, confirm = false) {
+export function showMessage(msg, title = null, confirm = false, bonusHTML = "") {
     return new Promise((resolve) => {
         const msgModal = document.getElementById("msg-modal");
         if (msgModal) {
@@ -91,7 +91,7 @@ export function showMessage(msg, title = null, confirm = false) {
             if (title) {
                 document.getElementById("modal-title").innerHTML = title;
             }
-            document.getElementById("modal-body").innerHTML = msg;
+            document.getElementById("modal-body").innerHTML = msg + bonusHTML;
             document.getElementById("hidden-msg-btn").click();
 
             const cancelBtn = document.getElementById("cancel-btn");
