c71fb9611512427775b6a5ae56b6b2a690c8172f
jnorindr <jade.norindr@gmail.com>
2025-05-07 10:16:48 +0200

    [SIM] Temporary fix for similarity form parameters

--- front/app/similarity/forms.py
+++ front/app/similarity/forms.py
@@ -90,13 +90,14 @@ class SimilarityForm(forms.Form):
             parameters.update(
                 {
                     "segswap_prefilter": self.cleaned_data.get(
-                        f"{algorithm}_cosine_preprocessing", True
+                        f"{algorithm}_{algorithm}_cosine_preprocessing", True
                     ),
                     "segswap_n": self.cleaned_data.get(
-                        f"{algorithm}_cosine_n_filter", 0
+                        f"{algorithm}_{algorithm}_cosine_n_filter", 0
                     ),
                 }
             )
+            print(parameters)
 
         # NOTE for watermarks only
         # if parameters.get("use_transpositions"):
