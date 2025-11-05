e7c5641cf7eff9605fcc9a6824b7ffa83289b060
paulhectork <paul.kervegan@hotmail.fr>
2025-11-05 15:46:27 +0100

    [ANNOTATIONS] manage to import mirador and mae

---
+++ front/annotations/index.js
@@ -0,0 +1,10 @@
+import Mirador from 'mirador';
+import Mae from "mirador-annotation-editor";
+
+// console.log(Mirador);
+console.log(Mirador);
+console.log(Mae);
+
+const config = { id: "miradorRoot" };
+
+// Mirador.viewer(config)
