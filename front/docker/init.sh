cf6e6eb68360628ba91a60bcb516c8060cd2c823
paulhectork <paul.kervegan@hotmail.fr>
2025-11-19 17:50:14 +0100

    [DOCKER] revert my changes to init.sh

--- front/docker/init.sh
+++ front/docker/init.sh
@@ -8,67 +8,74 @@ FRONT_ROOT="$(dirname "$DOCKER_DIR")"
 source "$FRONT_ROOT/scripts/utils.sh"
 export INSTALL_MODE="full_install"
 
+# FRONT_ENV="$FRONT_ROOT/app/config/.env"
+# FRONT_ENV_TEMPLATE="$FRONT_ROOT/app/config/.env.template"
+# DOCKER_ENV="$DOCKER_DIR/.env"
+# DOCKER_ENV_TEMPLATE="$DOCKER_DIR/.env.template"
+#
+# # backup DOCKER_ENV, copy the global .env to docker/ folder.
+# if [ -f "$DOCKER_ENV" ]; then cp "$DOCKER_ENV" "$DOCKER_ENV.backup"; fi;
+# if [ -f "$FRONT_ENV" ];
+# then cp "$FRONT_ENV" "$DOCKER_ENV";
+# else cp "$FRONT_ENV_TEMPLATE" "$DOCKER_ENV";
+# fi
+#
+# # $DOCKER_ENV is now a copy of $FRONT_ENV.
+# # merge it with defaults defined in $DOCKER_ENV_TEMPLATE
+# prev_line=""
+# add_env=""
+# while IFS="" read -r line; do
+#     if [[ $line =~ ^[^#]*= ]]; then
+#         param=$(echo "$line" | cut -d'=' -f1)
+#         val=$(echo "$line" | cut -d'=' -f2)
+#         desc=$(get_env_desc "$line" "$prev_line")
+#
+#         # 1. $param is in $DOCKER_ENV => update with value in $DOCKER_ENV_TEMPLATE
+#         if grep -Eq "^${param}=" "$DOCKER_ENV"; then
+#             sed_repl_inplace "s~^$param=.*$~$param=$val~" "$DOCKER_ENV";
+#         # 2. $param is not in $DOCKER_ENV_TEMPLATE => append param, default value and description from $DOCKER_ENV_TEMPLATE to $DOCKER_ENV.
+#         else
+#             add_env="$add_env\n# $desc\n$param=$val\n";
+#         fi
+#     fi;
+#     prev_line="$line"
+# done < "$DOCKER_ENV_TEMPLATE"
+# echo -e "$add_env" >> "$DOCKER_ENV"
+#
+# # place derived variables in $DOCKER_ENV at the end of the file
+# prev_line=""
+# env_default=""  # user-defined variables
+# env_ignore=""  # variables derived from others that should not be edited
+# while IFS="" read -r line; do
+#     if [[ $line =~ ^[^#]*= ]]; then
+#         param=$(echo "$line" | cut -d'=' -f1)
+#         val=$(echo "$line" | cut -d'=' -f2)
+#         desc=$(get_env_desc "$line" "$prev_line")
+#         full="# $desc\n$param=$val\n"
+#
+#         if [[ "$desc" =~ ^\s*IGNORE\s*$ ]]; then
+#             env_ignore="$env_ignore\n$full"
+#         else
+#             env_default="$env_default\n$full"
+#         fi
+#     fi
+#     prev_line="$line"
+# done < "$DOCKER_ENV"
+# echo -e "$env_default$env_ignore" > "$DOCKER_ENV"
+#
+# # TODO: make internal port constant and have only external PORTS (i.e only ports defined in docker/.env mutable)
+# # TODO: make FRONT_ENV ports hard coded when docker is enabled
+# # default_params won't be prompted to the user.
+# default_params=(
+#     "C_FORCE_ROOT" "AIIINOTATE_HOST" "AIIINOTATE_SCHEME" "MIRADOR_HOST"
+#     "MIRADOR_SCHEME" "MONGODB_HOST" "REDIS_HOST" "ALLOWED_HOSTS"
+# )
+# setup_env "$DOCKER_ENV" "${default_params[@]}" || error "Failed to setup $DOCKER_ENV."
+# source "$DOCKER_ENV"
+# exit 1
+
 FRONT_ENV="$FRONT_ROOT/app/config/.env"
-FRONT_ENV_TEMPLATE="$FRONT_ROOT/app/config/.env.template"
 DOCKER_ENV="$DOCKER_DIR/.env"
-DOCKER_ENV_TEMPLATE="$DOCKER_DIR/.env.template"
-
-# backup DOCKER_ENV, copy the global .env to docker/ folder.
-if [ -f "$DOCKER_ENV" ]; then cp "$DOCKER_ENV" "$DOCKER_ENV.backup"; fi;
-if [ -f "$FRONT_ENV" ];
-then cp "$FRONT_ENV" "$DOCKER_ENV";
-else cp "$FRONT_ENV_TEMPLATE" "$DOCKER_ENV";
-fi
-
-# $DOCKER_ENV is now a copy of $FRONT_ENV.
-# merge it with defaults defined in $DOCKER_ENV_TEMPLATE
-prev_line=""
-add_env=""
-while IFS="" read -r line; do
-    if [[ $line =~ ^[^#]*= ]]; then
-        param=$(echo "$line" | cut -d'=' -f1)
-        val=$(echo "$line" | cut -d'=' -f2)
-        desc=$(get_env_desc "$line" "$prev_line")
-
-        # 1. $param is in $DOCKER_ENV => update with value in $DOCKER_ENV_TEMPLATE
-        if grep -Eq "^${param}=" "$DOCKER_ENV"; then
-            echo "+++ $param"
-            sed_repl_inplace "s~^$param=.*$~$param=$val~" "$DOCKER_ENV";
-        # 2. $param is not in $DOCKER_ENV_TEMPLATE => append param, default value and description from $DOCKER_ENV_TEMPLATE to $DOCKER_ENV.
-        else
-            echo "--- $param"
-            add_env="$add_env\n# $desc\n$param=$val\n";
-        fi
-    fi;
-    prev_line="$line"
-done < "$DOCKER_ENV_TEMPLATE"
-echo -e "$add_env" >> "$DOCKER_ENV"
-
-# place derived variables in $DOCKER_ENV at the end of the file
-prev_line=""
-env_default=""  # user-defined variables
-env_ignore=""  # variables derived from others that should not be edited
-while IFS="" read -r line; do
-    if [[ $line =~ ^[^#]*= ]]; then
-        param=$(echo "$line" | cut -d'=' -f1)
-        val=$(echo "$line" | cut -d'=' -f2)
-        desc=$(get_env_desc "$line" "$prev_line")
-        full="# $desc\n$param=$val\n"
-
-        if [[ "$desc" =~ ^\s*IGNORE\s*$ ]]; then
-            env_ignore="$env_ignore\n$full"
-        else
-            env_default="$env_default\n$full"
-        fi
-    fi
-    prev_line="$line"
-done < "$DOCKER_ENV"
-echo -e "$env_default$env_ignore" > "$DOCKER_ENV"
-exit 1
-
-# TODO: move # IGNORE variables at the end of the script.
-
-# prompt user for the rest
 
 update_app_env "$FRONT_ENV" || error "Failed to setup $FRONT_ENV."
 source "$FRONT_ENV"
