13b34a729ee4370c61e7109837000e9e6c304389
paulhectork <paul.kervegan@hotmail.fr>
2025-11-24 13:52:42 +0100

    [SCRIPTS] run script ok

--- front/run.sh
+++ front/run.sh
@@ -2,15 +2,17 @@
 
 SUDO_PSW="$1"
 FRONT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ENV_FILE="$FRONT_DIR/app/config/.env"
 BIN="$FRONT_DIR"/venv/bin
 SCHEDULE_FILE="$FRONT_DIR/celery/celerybeat-schedule"
+ANNOTATIONS_DIR="$FRONT_DIR/annotations"
+ANNOTATIONS_BIN="$FRONT_DIR/annotations/node_modules/.bin"
 FRONT_PORT=${FRONT_PORT:-8000}
 
 declare -a PIDS=()
 
-source "$FRONT_DIR/app/config/.env"
+source "$ENV_FILE"
 source "$FRONT_DIR/scripts/utils.sh"
-
 
 get_password && echo || exit
 
@@ -23,6 +25,9 @@ if [ "$START_MODE" != "CHILD" ]; then
 
     trap cleanup INT TERM HUP
 fi
+
+# start all services just to be sure
+services_start
 
 "$BIN"/celery -A app.config.celery worker -B -c 1 --loglevel=INFO -P threads &
 CELERY_WORKER_PID=$!
@@ -44,11 +49,22 @@ PIDS+=($CANTALOUPE_PID)
 SAS_PID=$!
 PIDS+=($SAS_PID)
 
+"$ANNOTATIONS_BIN"/aiiinotate --env "$ENV_FILE" -- serve prod &
+AIIINOTATE_PID=$!
+PIDS+=($AIIINOTATE_PID)
+
+rm -rf "$ANNOTATIONS_DIR"/dist "$ANNOTATIONS_DIR"/.parcel-cache
+"$ANNOTATIONS_BIN"/parcel "$ANNOTATIONS_DIR"/src/index.html --port "$MIRADOR_PORT" &
+MIRADOR_PID=$!
+PIDS+=($MIRADOR_PID)
+
 color_echo cyan "Celery worker PID  $CELERY_WORKER_PID"
 color_echo cyan "Celery beat PID    $CELERY_BEAT_PID"
 color_echo cyan "Django PID         $DJANGO_PID"
 color_echo cyan "Cantaloupe PID     $CANTALOUPE_PID"
 color_echo cyan "SAS PID            $SAS_PID"
+color_echo cyan "AIIINOTATE PID     $AIIINOTATE_PID"
+color_echo cyan "MIRADOR PID        $MIRADOR_PID"
 
 if [ "$START_MODE" != "CHILD" ]; then
     color_echo magenta "Press Ctrl+C to stop all frontend processes"
