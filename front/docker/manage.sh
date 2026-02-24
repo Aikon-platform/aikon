2b0c301ec00a1ea1ff4fdcf4388cf80e7e6c8a60
paulhectork <paul.kervegan@hotmail.fr>
2026-02-24 14:56:40 +0100

    [DOCKER] manage.sh: create superuser

--- front/docker/manage.sh
+++ front/docker/manage.sh
@@ -4,23 +4,30 @@ set -e
 
 manage="uv --directory=/home/aikon/app run /home/aikon/app/manage.py"
 
-# wait 2sec for postgres to start
-sleep 2
-
 $manage collectstatic --noinput
 
 $manage makemigrations
 $manage migrate
 
-# source /home/aikon/app/config/.env
-# # Create superuser if it doesn't exist
-# echo "
-# from django.contrib.auth import get_user_model;
-# User = get_user_model();
-# username = '$POSTGRES_USER';
-# if not User.objects.filter(username=username).exists():
-#     User.objects.create_superuser(username, '$EMAIL_HOST_USER', '$POSTGRES_PASSWORD');
-#     print('Superuser created.');
-# else:
-#     print('Superuser already exists.');
-# " | /home/aikon/venv/bin/python /home/aikon/app/manage.py shell
+# check if the superuser exists: https://stackoverflow.com/a/29949548
+function superuser_exists() {
+    cmd="SELECT * FROM auth_user WHERE auth_user.username='$POSTGRES_USER';"
+    docker exec \
+        docker-db-1 \
+        psql -U $POSTGRES_USER -d $POSTGRES_DB -c "$cmd" -t \
+        | egrep .
+}
+
+# create superuser if it doesn't exist
+if [ $(superuser_exists | wc -l) -eq 0 ]; then
+    export DJANGO_SUPERUSER_USERNAME
+    DJANGO_SUPERUSER_USERNAME="$POSTGRES_USER"
+    export DJANGO_SUPERUSER_EMAIL
+    DJANGO_SUPERUSER_EMAIL="$EMAIL_HOST_USER"
+    export DJANGO_SUPERUSER_PASSWORD
+    DJANGO_SUPERUSER_PASSWORD="$POSTGRES_PASSWORD"
+    $manage createsuperuser --noinput
+    echo "Super user created:"
+    echo -e "          👤 $POSTGRES_USER"
+    echo -e "          🔑 $POSTGRES_PASSWORD"
+fi;
