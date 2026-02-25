a0202ae7bca5d024787bf0669640e7f5b820ccea
paulhectork <paul.kervegan@hotmail.fr>
2026-02-25 11:14:20 +0100

    [DOCKER] create_superuser_check ok

--- front/docker/manage.sh
+++ front/docker/manage.sh
@@ -11,23 +11,3 @@ $manage migrate
 
 $manage create_superuser_check
 
-# # NOTE: haha, docker pas accessible dans le conteneur
-# # check if the superuser exists: https://stackoverflow.com/a/29949548
-# function superuser_exists() {
-#     cmd="SELECT * FROM auth_user WHERE auth_user.username='$POSTGRES_USER';"
-#     docker exec \
-#         docker-db-1 \
-#         psql -U $POSTGRES_USER -d $POSTGRES_DB -c "$cmd" -t \
-#         | egrep .
-# }
-#
-# # create superuser if it doesn't exist
-# if [ $(superuser_exists | wc -l) -eq 0 ]; then
-#     export DJANGO_SUPERUSER_USERNAME
-#     DJANGO_SUPERUSER_USERNAME="$POSTGRES_USER"
-#     export DJANGO_SUPERUSER_EMAIL
-#     DJANGO_SUPERUSER_EMAIL="$EMAIL_HOST_USER"
-#     export DJANGO_SUPERUSER_PASSWORD
-#     DJANGO_SUPERUSER_PASSWORD="$POSTGRES_PASSWORD"
-#     $manage createsuperuser --noinput
-# fi;
