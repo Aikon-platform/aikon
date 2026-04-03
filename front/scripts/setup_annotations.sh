db6c5cb16e3e4b29c95a1aef7219d975e829c2d7
paulhectork <paul.kervegan@hotmail.fr>
2026-04-03 20:34:50 +0200

    [SETUP+ANNOTATIONS] aiiinotate->0.8.5 + fix annotations module dev setup

--- front/scripts/setup_annotations.sh
+++ front/scripts/setup_annotations.sh
@@ -2,6 +2,7 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd );
 ANNOTATIONS_DIR="$SCRIPT_DIR/../annotations";
+ANNOTATIONS_BIN="$ANNOTATIONS_DIR/node_modules/.bin"
 ENV_FILE="$SCRIPT_DIR/../app/config/.env";
 source "$SCRIPT_DIR"/utils.sh;
 
@@ -10,4 +11,5 @@ cd "$ANNOTATIONS_DIR";
 npm i --include=dev;  # in dev mode, devDependencies includes aiiinotate. in prod, aiiinotate is in its own Docker container.
 services_start;
 
-aiiinotate --env "$ENV_FILE" -- migrate apply;
+"$ANNOTATIONS_BIN"/dotenvx run -f "$ENV_FILE" \
+    -- "$ANNOTATIONS_BIN"/aiiinotate migrate apply;
