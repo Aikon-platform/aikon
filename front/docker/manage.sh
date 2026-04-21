51aed0e6aacbe1024c880cd428069329f4cdbcc1
Fouad Aouinti <aouinti.fouad@gmail.com>
2026-04-21 14:58:15 +0200

    minor changes

--- front/docker/manage.sh
+++ front/docker/manage.sh
@@ -6,7 +6,6 @@ manage="uv --directory=/home/aikon/app run /home/aikon/app/manage.py"
 
 $manage collectstatic --noinput
 
-# $manage makemigrations
-# $manage migrate
+$manage migrate
 
 $manage create_superuser_check
