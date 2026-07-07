f9cdd028c1f9dd6e828a0bf54a7359ab42037ebc
Segolene-Albouy <segolene.albouy@gmail.com>
2026-07-07 17:23:01 +0200

    [SETUP] add APP_URL_FROM_API

--- front/app/config/settings/dev.py
+++ front/app/config/settings/dev.py
@@ -6,6 +6,7 @@ API_URL = ENV.str(
 BASE_URL = ENV.str("BASE_URL", default=f"http://localhost:{APP_PORT}")
 APP_URL = BASE_URL
 APP_URL_FROM_DOCKER = ENV.str("APP_URL_FROM_DOCKER", default=APP_URL)
+APP_URL_FROM_API = ENV.str("APP_URL_FROM_API", default=APP_URL)
 CANTALOUPE_APP_URL = ENV.str(
     "CANTALOUPE_BASE_URI", default=f"http://localhost:{CANTALOUPE_PORT}"
 )
